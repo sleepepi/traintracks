fe3642bd9e4096893309ebfe659d8d208232a634
Remo Mueller <remosm@gmail.com>
2012-04-16 15:29:41 -0400

    Added applicant assurance to an earlier migration

--- db/migrate/20120416173438_add_submitted_at_to_applicants.rb
+++ db/migrate/20120416173438_add_submitted_at_to_applicants.rb
@@ -1,6 +1,7 @@
 class AddSubmittedAtToApplicants < ActiveRecord::Migration
   def up
     add_column :applicants, :submitted_at, :datetime
+    add_column :applicants, :assurance, :boolean, null: false, default: false
     Applicant.all.each do |applicant|
       unless applicant.year.blank?
         submitted_at = Time.local(applicant.year, "2") # February of the year to avoid timezone year shifts (if it was on Jan 1st)
@@ -10,6 +11,7 @@ class AddSubmittedAtToApplicants < ActiveRecord::Migration
   end
 
   def down
+    remove_column :applicants, :assurance
     remove_column :applicants, :submitted_at
   end
 end
--- db/migrate/20120416184603_add_assurance_to_applicant.rb
+++
@@ -1,5 +0,0 @@
-class AddAssuranceToApplicant < ActiveRecord::Migration
-  def change
-    add_column :applicants, :assurance, :boolean, null: false, default: false
-  end
-end
