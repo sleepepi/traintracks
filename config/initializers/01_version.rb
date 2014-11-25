8b8178c0e370de73eca168f976b54bed26ad2910
Remo Mueller <remosm@gmail.com>
2014-11-25 16:10:49 -0500

    Version bump to 0.10.7.pre

--- config/initializers/01_version.rb
+++ config/initializers/01_version.rb
@@ -2,8 +2,8 @@ module TrainingGrant
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 10
-    TINY = 6
-    BUILD = nil # nil, "pre", "beta1", "beta2", "rc", "rc2"
+    TINY = 7
+    BUILD = "pre" # nil, "pre", "beta1", "beta2", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
