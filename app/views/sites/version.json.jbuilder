1952fc9bee1bd9fac9f413e8ea696806e2688858
Remo Mueller <remosm@gmail.com>
2016-01-12 09:58:28 -0500

    Added missing minor version attribute to version JSON API

--- app/views/sites/version.json.jbuilder
+++ app/views/sites/version.json.jbuilder
@@ -1,6 +1,7 @@
 json.version do
   json.string TrainingGrant::VERSION::STRING
   json.major TrainingGrant::VERSION::MAJOR
+  json.minor TrainingGrant::VERSION::MINOR
   json.tiny TrainingGrant::VERSION::TINY
   json.build TrainingGrant::VERSION::BUILD
 end
--- test/controllers/sites_controller_test.rb
+++ test/controllers/sites_controller_test.rb
@@ -1,45 +1,40 @@
 require 'test_helper'
 
 class SitesControllerTest < ActionController::TestCase
-
-  setup do
-    # Nothing
-  end
-
-  test "should get about" do
+  test 'should get about' do
     get :about
     assert_response :success
   end
 
-  test "should get index" do
+  test 'should get index' do
     login(users(:administrator))
     get :dashboard
     assert_response :success
   end
 
-  test "should get contact" do
+  test 'should get contact' do
     get :contact
     assert_response :success
   end
 
-  test "should get forgot my password" do
+  test 'should get forgot my password' do
     get :forgot_my_password
     assert_response :success
   end
 
-  test "should get version" do
+  test 'should get version' do
     get :version
     assert_response :success
   end
 
-  test "should get version as json" do
+  test 'should get version as json' do
     get :version, format: 'json'
     version = JSON.parse(response.body)
     assert_equal TrainingGrant::VERSION::STRING, version['version']['string']
     assert_equal TrainingGrant::VERSION::MAJOR, version['version']['major']
+    assert_equal TrainingGrant::VERSION::MINOR, version['version']['minor']
     assert_equal TrainingGrant::VERSION::TINY, version['version']['tiny']
     assert_equal TrainingGrant::VERSION::BUILD, version['version']['build']
     assert_response :success
   end
-
 end
