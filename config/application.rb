faa800a7bc7f1f37665e0906afeaa7d0dbb33299
Remo Mueller <remosm@gmail.com>
2018-04-17 12:56:38 -0400

    Migrate to new gems.rb format

--- config/application.rb
+++ config/application.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails'
+require "rails"
 # Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_view/railtie'
-# require 'action_cable/engine'
-require 'sprockets/railtie'
-require 'rails/test_unit/railtie'
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
+# Require the gems listed in gems.rb, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
@@ -26,7 +26,7 @@ module TrainingGrant
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rails time:zones" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Eastern Time (US & Canada)'
+    config.time_zone = "Eastern Time (US & Canada)"
 
     # Overwrite Rails errors to use Bootstrap CSS classes
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
