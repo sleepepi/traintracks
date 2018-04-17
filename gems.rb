f34a5d652fc9749921011102403617a93ba4607a
Remo Mueller <remosm@gmail.com>
2018-04-17 12:57:13 -0400

    Prefer double quotes

--- gems.rb
+++ gems.rb
@@ -1,37 +1,37 @@
 # frozen_string_literal: true
 
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem 'rails',                '5.1.1'
+gem "rails",                "5.1.1"
 
 # Database Adapter
-gem 'mysql2',               '0.4.6'
+gem "mysql2",               "0.4.6"
 
 # Gems used by project
-gem 'autoprefixer-rails'
-gem 'bootstrap-sass',       '~> 3.3.7'
-gem 'carrierwave',          '~> 1.1.0'
-gem 'devise',               '~> 4.3.0'
-gem 'figaro',               '~> 1.1.1'
-gem 'haml',                 '~> 5.0.1'
-gem 'kaminari',             '~> 1.0.1'
+gem "autoprefixer-rails"
+gem "bootstrap-sass",       "~> 3.3.7"
+gem "carrierwave",          "~> 1.1.0"
+gem "devise",               "~> 4.3.0"
+gem "figaro",               "~> 1.1.1"
+gem "haml",                 "~> 5.0.1"
+gem "kaminari",             "~> 1.0.1"
 
 # Rails Defaults
-gem 'coffee-rails',         '~> 4.2'
-gem 'sass-rails',           '~> 5.0'
-gem 'uglifier',             '>= 1.3.0'
+gem "coffee-rails",         "~> 4.2"
+gem "sass-rails",           "~> 5.0"
+gem "uglifier",             ">= 1.3.0"
 
-gem 'jbuilder',             '~> 2.5'
-gem 'jquery-rails',         '~> 4.3.1'
-gem 'turbolinks',           '~> 5'
+gem "jbuilder",             "~> 2.5"
+gem "jquery-rails",         "~> 4.3.1"
+gem "turbolinks",           "~> 5"
 
 # Testing
 group :test do
-  gem 'minitest'
-  gem 'rails-controller-testing'
-  gem 'simplecov',          '~> 0.14.1', require: false
+  gem "minitest"
+  gem "rails-controller-testing"
+  gem "simplecov",          "~> 0.14.1", require: false
 end
 
 group :development do
-  gem 'web-console', '~> 3.0'
+  gem "web-console", "~> 3.0"
 end
