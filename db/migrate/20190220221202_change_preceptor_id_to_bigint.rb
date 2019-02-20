094fec61ba0f4168ba092ebe28f3d1bd4cf5a65e
Remo Mueller <remosm@gmail.com>
2019-02-20 17:13:56 -0500

    Update preceptor_id primary and foreign keys to bigint

---
+++ db/migrate/20190220221202_change_preceptor_id_to_bigint.rb
@@ -0,0 +1,17 @@
+class ChangePreceptorIdToBigint < ActiveRecord::Migration[6.0]
+  def up
+    change_column :preceptors, :id, :bigint
+
+    change_column :applicants, :preferred_preceptor_id, :bigint
+    change_column :applicants, :primary_preceptor_id, :bigint
+    change_column :applicants, :secondary_preceptor_id, :bigint
+  end
+
+  def down
+    change_column :preceptors, :id, :integer
+
+    change_column :applicants, :preferred_preceptor_id, :integer
+    change_column :applicants, :primary_preceptor_id, :integer
+    change_column :applicants, :secondary_preceptor_id, :integer
+  end
+end
